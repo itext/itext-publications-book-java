c9338d3b574806073f16eb9c627eef9dde230d8f
Alexey Subach <alexey.subach@duallab.com>
2016-08-12 02:11:01 +0300

    Fix sample in accordance with PdfImageXObject image type detection functionality
    DEVSIX-624

--- src/test/java/com/itextpdf/samples/book/part4/chapter15/Listing_15_30_ExtractImages.java
+++ src/test/java/com/itextpdf/samples/book/part4/chapter15/Listing_15_30_ExtractImages.java
@@ -8,16 +8,17 @@
 package com.itextpdf.samples.book.part4.chapter15;
 
 import com.itextpdf.io.source.ByteArrayOutputStream;
-import com.itextpdf.kernel.pdf.canvas.parser.listener.IEventListener;
-import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.IEventListener;
 import com.itextpdf.test.annotations.type.SampleTest;
 
-import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -27,12 +28,13 @@ import org.xml.sax.SAXException;
 @Category(SampleTest.class)
 public class Listing_15_30_ExtractImages {
     public static final String DEST
-            = "./target/test/resources/book/part4/chapter15/Listing_15_30_ExtractImages%s.%s";
+            = "./target/test/resources/book/part4/chapter15/Img%s.%s";
     public static final String IMAGE_TYPES
             = "./src/test/resources/book/part3/chapter10/cmp_Listing_10_09_ImageTypes.pdf";
 
     @Test
     public void manipulatePdf() throws IOException, SQLException, ParserConfigurationException, SAXException {
+        new File(DEST).getParentFile().mkdirs();
         PdfDocument pdfDoc = new PdfDocument(new PdfReader(IMAGE_TYPES), new PdfWriter(new ByteArrayOutputStream()));
         IEventListener listener = new Listing_15_31_MyImageRenderListener(DEST);
         PdfCanvasProcessor parser = new PdfCanvasProcessor(listener);
--- src/test/java/com/itextpdf/samples/book/part4/chapter15/Listing_15_31_MyImageRenderListener.java
+++ src/test/java/com/itextpdf/samples/book/part4/chapter15/Listing_15_31_MyImageRenderListener.java
@@ -8,13 +8,11 @@
 package com.itextpdf.samples.book.part4.chapter15;
 
 
-import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.pdf.canvas.parser.data.IEventData;
-import com.itextpdf.kernel.pdf.canvas.parser.listener.IEventListener;
 import com.itextpdf.kernel.pdf.canvas.parser.EventType;
+import com.itextpdf.kernel.pdf.canvas.parser.data.IEventData;
 import com.itextpdf.kernel.pdf.canvas.parser.data.ImageRenderInfo;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.IEventListener;
 import com.itextpdf.kernel.pdf.xobject.PdfImageXObject;
-
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,7 +24,6 @@ public class Listing_15_31_MyImageRenderListener implements IEventListener {
     protected String path;
 
     protected String extension;
-    protected int i = 1;
 
     public Listing_15_31_MyImageRenderListener(String path) {
         this.path = path;
@@ -43,50 +40,14 @@ public class Listing_15_31_MyImageRenderListener implements IEventListener {
                     if (image == null) {
                         return;
                     }
-                    byte[] imageByte = null;
-                    try {
-                        imageByte = image.getImageBytes(true);
-                        switch (ImageDataFactory.create(imageByte).getOriginalType()) {
-                            case JPEG:
-                                extension = "jpg";
-                                break;
-                            case PNG:
-                                extension = "png";
-                                break;
-                            case GIF:
-                                extension = "gif";
-                                break;
-                            case BMP:
-                                extension = "bmp";
-                                break;
-                            case TIFF:
-                                extension = "tif";
-                                break;
-                            case WMF:
-                                extension = "wmf";
-                                break;
-                            case JPEG2000:
-                                extension = "jp2";
-                                break;
-                            case JBIG2:
-                                extension = "jbig2";
-                                break;
-
-                            default:
-                                extension = "jpg";
-                        }
-                    } catch (com.itextpdf.io.IOException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    if (imageByte == null) {
-                        return;
-                    }
-                    filename = String.format(path, i++, extension);
+                    byte[] imageByte = image.getImageBytes(true);
+                    extension = image.identifyImageFileExtension();
+                    filename = String.format(path, image.getPdfObject().getIndirectReference().getObjNumber(), extension);
                     os = new FileOutputStream(filename);
                     os.write(imageByte);
                     os.flush();
                     os.close();
-                } catch (IOException e) {
+                } catch (com.itextpdf.io.IOException | IOException e) {
                     System.out.println(e.getMessage());
                 }
                 break;
