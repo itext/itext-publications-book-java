cbdd5d730fa6eb22f5189c01fafc4bf526d951a7
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2016-08-17 16:45:13 +0300

    Fix a sample
    Fix invalid intensities of cmyk colorants and grayscale

--- src/test/java/com/itextpdf/samples/book/part3/chapter10/Listing_10_01_DeviceColor.java
+++ src/test/java/com/itextpdf/samples/book/part3/chapter10/Listing_10_01_DeviceColor.java
@@ -7,21 +7,20 @@
 
 package com.itextpdf.samples.book.part3.chapter10;
 
-import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.kernel.color.Color;
 import com.itextpdf.kernel.color.DeviceCmyk;
 import com.itextpdf.kernel.color.DeviceGray;
 import com.itextpdf.kernel.color.DeviceRgb;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.test.annotations.type.SampleTest;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.samples.GenericTest;
+import com.itextpdf.test.annotations.type.SampleTest;
+import org.junit.experimental.categories.Category;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
-
-import org.junit.experimental.categories.Category;
 
 @Category(SampleTest.class)
 public class Listing_10_01_DeviceColor extends GenericTest {
@@ -45,27 +44,27 @@ public class Listing_10_01_DeviceColor extends GenericTest {
         colorRectangle(canvas, Color.BLACK, 416, 770, 36, 36);
         colorRectangle(canvas, Color.LIGHT_GRAY, 470, 770, 36, 36);
         // CMYK Colors
-        colorRectangle(canvas, new DeviceCmyk(0x00, 0x00, 0x00, 0x00), 36, 716, 36, 36);
-        colorRectangle(canvas, new DeviceCmyk(0x00, 0x00, 0xFF, 0x00), 90, 716, 36, 36);
-        colorRectangle(canvas, new DeviceCmyk(0x00, 0x00, 0xFF, 0xFF), 144, 716, 36, 36);
-        colorRectangle(canvas, new DeviceCmyk(0x00, 0xFF, 0x00, 0x00), 198, 716, 36, 36);
+        colorRectangle(canvas, new DeviceCmyk(0, 0, 0, 0), 36, 716, 36, 36);
+        colorRectangle(canvas, new DeviceCmyk(0, 0, 100, 0), 90, 716, 36, 36);
+        colorRectangle(canvas, new DeviceCmyk(0, 0, 100, 100), 144, 716, 36, 36);
+        colorRectangle(canvas, new DeviceCmyk(0, 100, 0, 0), 198, 716, 36, 36);
         colorRectangle(canvas, new DeviceCmyk(0f, 1f, 0f, 0.5f), 252, 716, 36, 36);
         colorRectangle(canvas, new DeviceCmyk(1f, 0f, 0f, 0f), 306, 716, 36, 36);
         colorRectangle(canvas, new DeviceCmyk(1f, 0f, 0f, 0.5f), 360, 716, 36, 36);
         colorRectangle(canvas, new DeviceCmyk(1f, 0f, 0f, 1f), 416, 716, 36, 36);
         colorRectangle(canvas, new DeviceCmyk(0f, 0f, 0f, 1f), 470, 716, 36, 36);
         // Gray color
-        colorRectangle(canvas, new DeviceGray(0x20), 36, 662, 36, 36);
-        colorRectangle(canvas, new DeviceGray(0x40), 90, 662, 36, 36);
-        colorRectangle(canvas, new DeviceGray(0x60), 144, 662, 36, 36);
+        colorRectangle(canvas, new DeviceGray(0.125f), 36, 662, 36, 36);
+        colorRectangle(canvas, new DeviceGray(0.25f), 90, 662, 36, 36);
+        colorRectangle(canvas, new DeviceGray(0.375f), 144, 662, 36, 36);
         colorRectangle(canvas, new DeviceGray(0.5f), 198, 662, 36, 36);
         colorRectangle(canvas, new DeviceGray(0.625f), 252, 662, 36, 36);
         colorRectangle(canvas, new DeviceGray(0.75f), 306, 662, 36, 36);
         colorRectangle(canvas, new DeviceGray(0.825f), 360, 662, 36, 36);
-        colorRectangle(canvas, Color.DARK_GRAY, 416, 662, 36, 36);
-        colorRectangle(canvas, Color.LIGHT_GRAY, 470, 662, 36, 36);
+        colorRectangle(canvas, DeviceGray.BLACK, 416, 662, 36, 36);
+        colorRectangle(canvas, DeviceGray.WHITE, 470, 662, 36, 36);
         // Alternative ways to color the rectangle
-        canvas.setFillColorRgb(0x00, 0x80, 0x80);
+        canvas.setFillColorRgb(0, 0.5f, 0.5f);
         canvas.rectangle(36, 608, 36, 36);
         canvas.fillStroke();
         canvas.setFillColorRgb(0.5f, 0.25f, 0.60f);
@@ -74,7 +73,7 @@ public class Listing_10_01_DeviceColor extends GenericTest {
         canvas.setFillColorGray(0.5f);
         canvas.rectangle(144, 608, 36, 36);
         canvas.fillStroke();
-        canvas.setFillColorCmyk(0xFF, 0xFF, 0x00, 0x80);
+        canvas.setFillColorCmyk(1, 1, 0, 0.5f);
         canvas.rectangle(198, 608, 36, 36);
         canvas.fillStroke();
         canvas.setFillColorCmyk(0f, 1f, 1f, 0.5f);
